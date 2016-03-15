818238f2fa80c6c8be728afa95744d4295ca4d52
Asaf Golan <asafgolan@Asafs-MacBook-Pro.local>
2016-03-15 14:17:21 +0200

    typed model

--- models/exhibit.model.js
+++ models/exhibit.model.js
@@ -1,12 +1,16 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 var collectionName = 'exhibits';
-
+//number 1  translated description.
+//number 2 image
+//number 3 video
 var exhibitModel = new Schema({
     title: {type: String},
     content:[{
       title: {type: String},
+      type:{type: Number},
       language: {type: String},
+      //check string limitations
       description: {type: String},
       url: {type: String}
     }]
